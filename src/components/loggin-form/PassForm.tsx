99d6d708033a170d7ae757d692ca54a42df65a03
Fernando de Row <ferdr89dev@gmail.com>
2022-04-14 10:26:13 -0300

    Probando repo en nueva pc

--- src/components/loggin-form/PassForm.tsx
+++ src/components/loggin-form/PassForm.tsx
@@ -20,23 +20,23 @@ function PassForm() {
   //   }
   // };
 
-  return (
-    <form onSubmit={handleSubmit} className={css.form}>
-      <TextField
-        inputType="password"
-        inputName="password"
-        labelText="Contraseña"
-        holder="Ingresá aquí tu password"
-        fieldSetHeight="90px"
-      />
-      <MyButton bgc={"var(--btn-bg1)"}>
-        <Text tag="text-bold" fsize="16px">
-          Ingresar
-        </Text>
-      </MyButton>
-      <a className={css.link}>OLVIDÉ MI CONTRASEÑA</a>
-    </form>
-  );
+  // return (
+  //   <form onSubmit={handleSubmit} className={css.form}>
+  //     <TextField
+  //       inputType="password"
+  //       inputName="password"
+  //       labelText="Contraseña"
+  //       holder="Ingresá aquí tu password"
+  //       fieldSetHeight="90px"
+  //     />
+  //     <MyButton bgc={"var(--btn-bg1)"}>
+  //       <Text tag="text-bold" fsize="16px">
+  //         Ingresar
+  //       </Text>
+  //     </MyButton>
+  //     <a className={css.link}>OLVIDÉ MI CONTRASEÑA</a>
+  //   </form>
+  // );
 }
 
 export { PassForm };
